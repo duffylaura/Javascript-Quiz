578e69516cfbdf128b10b82790a3dc0977a10170
Coffee Pawz <lauralynneduffy@gmail.com>
2022-10-03 02:07:16 -0400

    Tried several different ways of organizing the quiz; haven't figured anything out yet to meet criteria.

---
+++ assets/questions.js
@@ -0,0 +1,57 @@
+var myQuestions = [
+
+    {
+        question: "Commonly used data types do NOT include:",
+        answers: [
+            "Strings",
+            "Booleans",
+            "Alerts",
+            "Numbers"
+        ], 
+        correctAnswer: "Alerts",
+    },
+
+    {
+        question: "The condition in an if/else statement is enclosed with",
+        answers: [
+            "Quotes",
+            "Curly Brackets",
+            "Parenthesis",
+            "Square Brackets"
+        ],
+        correctAnswer:"Parenthesis",
+    },
+
+    {
+        question: "Arrays in JavaScript can be used to store ________________.",
+        answers: [
+            "Numbers and Strings", 
+            "Other Arrays", 
+            "Booleans", 
+            "All of the Above"
+        ],
+        correctAnswer:"All of the Above",
+    },
+
+    {
+        question: "String values must be enclosed within ______________ when being assigned to variables.",
+        answers: [
+            "Commas",
+            "Curly Brackets",
+            "Quotes",
+            "Parenthesis"
+        ],
+        correctAnswer: "Quotes",
+    },
+
+    {
+        question: "A very useful tool used during development and debugging for printing content to the debugger is:",
+        answers: [
+            "JavaScript",
+            "Terminal/Bash",
+            "For Loops",
+            "Console.Log"
+        ],
+        correctAnswer: "Console.Log",
+    },
+  ];
